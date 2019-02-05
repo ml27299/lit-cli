ae98b2578b227b0b8e7744bf94a8c0fc2d267a4e
Macario Lara <ml27299@gmail.com>
2019-02-04 20:36:08 -0600

    update

--- install.sh
+++ install.sh
@@ -1,10 +1,9 @@
-TAG=$1;
+TAG=$1
 
 if (( $EUID != 0 )); then
-    echo "Please run command as root.";
-    exit;
+    echo "Please run command as root."
+    exit
 fi
 
-DOWNLOADER="https://raw.githubusercontent.com/ml27299/lit-cli/master/godownloader.sh";
-echo "yo";
-eval "curl -sL -o- ${DOWNLOADER} | bash -s -- -b /usr/local/bin $TAG"
+DOWNLOADER="https://raw.githubusercontent.com/ml27299/lit-cli/master/godownloader.sh"
+curl -sL -o- ${DOWNLOADER} | bash -s -- -b /usr/local/bin $TAG
