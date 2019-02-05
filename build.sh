31c8bea236c9409f5ea7785908e33e1c5025f91a
Macario Lara <ml27299@gmail.com>
2019-02-04 20:56:01 -0600

    update

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-export GITHUB_TOKEN="e6b1c967009056061b8347e9c8d30858f4621b47"
+export GITHUB_TOKEN=e6b1c967009056061b8347e9c8d30858f4621b47
 
 eval "git tag -a v$1 -m \"update\""
 eval "goreleaser"
