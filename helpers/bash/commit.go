42f544d1e4c2e818302cd149ca9e162ee22c27b1
Macario Lara <ml27299@gmail.com>
2019-02-11 01:27:52 -0600

    update

--- helpers/bash/commit.go
+++ helpers/bash/commit.go
@@ -1,8 +1,41 @@
 package bash
 import (
+	"fmt"
 	"os"
 	"os/exec"
 )
+
+func hasChanges() (bool, error) {
+	has_changes, err := exec.Command("git", "diff", "--exit-code").Output()
+	if err != nil && err.Error() != "exit status 1" {
+		return false, err
+	}
+
+	if string(has_changes[:]) == "" {
+		has_changes, err = exec.Command("git", "diff", "--cache", "--exit-code").Output()
+		if err != nil && err.Error() != "exit status 1" {
+			return false, err
+		}
+	}
+
+	if string(has_changes[:]) == "" {
+
+		untracked_files, err := exec.Command("git", "ls-files", "--other", "--exclude-standard", "--directory").Output()
+		if err != nil && err.Error() != "exit status 1" {
+			return false, err
+		}
+
+		if string(untracked_files[:]) == "" {
+			return false, nil
+		}
+
+		fmt.Println("Untacked files...")
+		fmt.Println(string(untracked_files))
+		return false, nil
+	}
+
+	return true, nil
+}
 
 func Commit(path string, args []string) error {	
 	current_path, err := os.Getwd()
@@ -11,6 +44,15 @@ func Commit(path string, args []string) error {
 	if err != nil {
         return err
     }
+
+    changes, err := hasChanges()
+    if err != nil {
+		return err
+	}
+
+	if !changes {
+		return nil
+	}
 
     args = append([]string{"commit"}, args...)
     cmd := exec.Command("git", args...)
@@ -38,6 +80,15 @@ func CommitViaBash(path string, args string) error {
 	if err != nil {
         return err
     }
+
+    changes, err := hasChanges()
+    if err != nil {
+		return err
+	}
+
+	if !changes {
+		return nil
+	}
 
     cmd := exec.Command("sh", "-c", "git commit "+args)
 
