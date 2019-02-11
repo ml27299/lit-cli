2171d1aea6abaa47661bb2c49eeeb14b54ef753d
Macario Lara <ml27299@gmail.com>
2019-02-11 15:47:07 -0600

    update

--- helpers/parser/main.go
+++ helpers/parser/main.go
@@ -5,7 +5,6 @@ import(
 	"io/ioutil"
 	"../paths"
 	"errors"
-	"../resources"
 	"os"
 )
 
@@ -18,68 +17,39 @@ func Config() (ParseInfo, error) {
 		return response, err
 	}
 
-	if _, err := os.Stat(dir+"/lit.link.json"); os.IsNotExist(err) {
-
-		data, err := resources.Asset("lit.link.json")
-		file, err := os.Create(dir+"/lit.link.json")
-
-		defer file.Close()
-
-		if err != nil {
-			return response, err
-		}
-
-		_, err = file.Write(data)
-		if err != nil {
-			return response, err
-		}
-	}
-
-	if _, err := os.Stat(dir+"/lit.module.json"); os.IsNotExist(err) {
-
-		data, err := resources.Asset("lit.module.json")
-		file, err := os.Create(dir+"/lit.module.json")
-
-		defer file.Close()
-
-		if err != nil {
-			return response, err
-		}
-
-		_, err = file.Write(data)
-		if err != nil {
-			return response, err
-		}
-	}
-	
-	//config_json_string, err := ioutil.ReadFile(dir+"/lit.config.json")
-	links_json_string, err := ioutil.ReadFile(dir+"/lit.link.json")
-	modules_json_string, err := ioutil.ReadFile(dir+"/lit.module.json")
-	if err != nil {
-		return response, err 
-	}
-
-	// if !json.Valid([]byte(config_json_string)) {
-	// 	return response, errors.New("config json is not valid...")
-	// }
-
-	if !json.Valid([]byte(links_json_string)) {
-		return response, errors.New("link json is not valid...")
-	}
-
-	if !json.Valid([]byte(modules_json_string)) {
-		return response, errors.New("module json is not valid...")
-	}
-	
 	var (
 		linkItems []LinkItem
 		modules []GitModule
-		//config ConfigInfo
 	)
 
-	//json.Unmarshal(config_json_string, &config)
-	json.Unmarshal([]byte(links_json_string), &linkItems)
-	json.Unmarshal([]byte(modules_json_string), &modules)
+	if _, err := os.Stat(dir+"/lit.link.json"); err == nil {
+
+		links_json_string, err := ioutil.ReadFile(dir+"/lit.link.json")	
+		if err != nil {
+			return response, err 
+		}
+
+		if !json.Valid([]byte(links_json_string)) {
+			return response, errors.New("link json is not valid...")
+		}
+
+		json.Unmarshal([]byte(links_json_string), &linkItems)
+	}
+
+
+	if _, err := os.Stat(dir+"/lit.module.json"); err == nil {
+
+		modules_json_string, err := ioutil.ReadFile(dir+"/lit.module.json")	
+		if err != nil {
+			return response, err 
+		}
+	
+		if !json.Valid([]byte(modules_json_string)) {
+			return response, errors.New("module json is not valid...")
+		}
+
+		json.Unmarshal([]byte(modules_json_string), &modules)
+	}
 
 	return ParseInfo{
 		LinkItems: linkItems,
